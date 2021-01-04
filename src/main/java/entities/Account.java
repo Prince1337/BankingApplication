296c4738f6f4d3dcd5684444ad4fa956b51743d7
Prince1337 <prince.pieritz@gmail.com>
2021-01-04 15:59:02 +0100

    feature: add account class methods test: add account class tests

--- src/main/java/entities/Account.java
+++ src/main/java/entities/Account.java
@@ -1,4 +1,35 @@
 package entities;
 
+import java.math.BigDecimal;
+
 public class Account {
+  private BigDecimal balance;
+
+  public Account(BigDecimal balance) {
+    this.balance = balance;
+  }
+
+  public BigDecimal getBalance() {
+    return balance;
+  }
+
+  public void add(BigDecimal amount){
+    if(isNotValid(amount)){
+      amount = BigDecimal.valueOf(0);
+    }
+      this.balance = balance.add(amount);
+  }
+
+  private boolean isNotValid(BigDecimal amount) {
+    return amount.compareTo(BigDecimal.valueOf(0)) < 0 ||
+        amount.compareTo(BigDecimal.valueOf(20000)) > 0;
+  }
+
+  public void subtract(BigDecimal amount){
+    if(isNotValid(amount)){
+      amount = BigDecimal.valueOf(0);
+    }
+    this.balance = balance.subtract(amount);
+  }
+
 }
---
+++ src/test/java/entities/AccountTest.java
@@ -0,0 +1,120 @@
+package entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+  @Test
+  @DisplayName("Create account")
+  void createAccount() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    Account testAccount = new Account(initialBalance);
+    assertNotNull(testAccount);
+  }
+
+  @Test
+  @DisplayName("add one euro")
+  void addOneEuro() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(1);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.add(amount);
+
+    assertEquals(initialBalance.add(amount), testAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("add Zero euros")
+  void addZeroEuros() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(0);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.add(amount);
+
+    assertEquals(initialBalance.add(amount), testAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("add negative amount")
+  void addNegativeAmount() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(-100);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.add(amount);
+
+    assertEquals(initialBalance, testAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("add more than highest amount")
+  void addMoreThanHighestAmount() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(20001);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.add(amount);
+
+    assertEquals(initialBalance, testAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("subtract one euro")
+  void subtractOneEuro() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(1);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.subtract(amount);
+
+    assertEquals(initialBalance.subtract(amount), testAccount.getBalance());
+
+  }
+
+  @Test
+  @DisplayName("subtract zero euros")
+  void subtractZeroEuros() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(0);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.subtract(amount);
+
+    assertEquals(initialBalance.subtract(amount), testAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("subtract negative amount")
+  void subtractNegativeAmount() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(-100);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.subtract(amount);
+
+    assertEquals(initialBalance, testAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("subtract more than highest amount")
+  void subtractMoreThanHighestAmount() {
+    BigDecimal initialBalance = BigDecimal.valueOf(100);
+    BigDecimal amount = BigDecimal.valueOf(20001);
+    Account testAccount = new Account(initialBalance);
+
+    testAccount.subtract(amount);
+
+    assertEquals(initialBalance, testAccount.getBalance());
+  }
+
+}
